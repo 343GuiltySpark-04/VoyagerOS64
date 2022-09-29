9731b39563e70c7e0f2cfdda2982514e0e36e05c
Tristan Adams <killerdragonxs@gmail.com>
2022-09-29 17:16:28 -0400

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -2,6 +2,15 @@
 #include <stddef.h>
 #include "include/limine.h"
 
+#define     White   "\033[1;00m"
+#define     Red     "\033[1;31m"
+#define     Green   "\033[1;32m"
+#define     Yellow  "\033[1;33m"
+#define     Blue    "\033[1;34m"
+#define     Purple  "\033[1;35m"
+#define     Cyan    "\033[1;36m"
+#define     Black   "\033[1;37m
+
 // The Limine requests can be placed anywhere, but it is important that
 // the compiler does not optimise them away, so, usually, they should
 // be made volatile or equivalent.
