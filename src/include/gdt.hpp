07fa5abcbe183e6d07c290d1ddbc7c54b8768964
Tristan Adams <killerdragonxs@gmail.com>
2022-09-30 15:12:33 -0400

    Update gdt.hpp

--- src/include/gdt.hpp
+++ src/include/gdt.hpp
@@ -2,11 +2,18 @@
 
 #define GDTAccessDPL(n) (n << 5)
 
+namespace GDTAccessFlag
+{
 
-namespace GDTAccessFlag{
+    enum GDTAccessFlag
+    {
 
- enum GDTAccessFlag{
+        ReadWrite = (2 << 1),
+        DC = (1 << 2),
+        Execute = (1 << 3),
+        Segments = (1 << 4),
+        Present = (1 << 7)
 
- };
+    };
 
 }
