a4a264de07b159ce528fced3b34e8d2bed471291
Tristan Adams <killerdragonxs@gmail.com>
2022-09-30 15:18:59 -0400

    e

--- src/include/gdt.hpp
+++ src/include/gdt.hpp
@@ -17,3 +17,12 @@ namespace GDTAccessFlag
     };
 
 }
+
+#define GDTKernelBaseSelector 0x08
+#define GDTUserBaseSelector 0x18
+#define GDTTSSSegment 0x30
+
+#define GDTAccessKernelCode (GDTAccessFlag::ReadWrite | GDTAccessFlag::Execute | GDTAccessFlag::Segments | GDTAccessFlag::Present)
+#define GDTAccessKernelData (GDTAccessFlag::ReadWrite | GDTAccessFlag::Segments | GDTAccessFlag::Present)
+#define GDTAccessUserCode (GDTAccessFlag::ReadWrite | GDTAccessFlag::Execute | GDTAccessFlag::Segments | GDTAccessDPL(3) | GDTAccessFlag::Present)
+#define GDTAccessUserData (GDTAccessFlag::ReadWrite | GDTAccessFlag::Segments | GDTAccessDPL(3) | GDTAccessFlag::Present)
