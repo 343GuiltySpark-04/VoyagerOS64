55d08b78622d4593c7c25dfb50110e5310e10056
Tristan Adams <killerdragonxs@gmail.com>
2022-10-06 11:46:54 -0400

    Update idt.c

--- src/idt.c
+++ src/idt.c
@@ -1,6 +1,7 @@
 #include "include/global_defs.h"
 #include <stdint.h>
 #include "include/idt.h"
+#include <stdbool.h>
 
 __attribute__((aligned(0x10))) static idt_entry_t idt[256]; // Create an array of IDT entries; aligned for performance
 typedef struct
@@ -39,4 +40,21 @@ void idt_set_descriptor(uint8_t vector, void *isr, uint8_t flags)
     descriptor->base_mid = ((uint64_t)isr >> 16) & 0xFFFF;
     descriptor->base_high = ((uint64_t)isr >> 32) & 0xFFFFFFFF;
     descriptor->reserved = 0;
+}
+
+
+extern void* isr_stub_table[];
+ 
+void idt_init(void);
+void idt_init() {
+    idtr.base = (uintptr_t)&idt[0];
+    idtr.limit = (uint16_t)sizeof(idt_entry_t) * 256 - 1;
+ 
+    for (uint8_t vector = 0; vector < 32; vector++) {
+        idt_set_descriptor(vector, isr_stub_table[vector], 0x8E);
+        vectors[vector] = true;
+    }
+ 
+    __asm__ volatile ("lidt %0" : : "m"(idtr)); // load the new IDT
+    __asm__ volatile ("sti"); // set the interrupt flag
 }
