eb76d41cd2842ea5d01ca13c41e0546c08c243f6
Tristan Adams <killerdragonxs@gmail.com>
2022-10-06 11:49:33 -0400

    Update isr.asm

--- src/isr.asm
+++ src/isr.asm
@@ -4,13 +4,13 @@
 %macro isr_err_stub 1
 isr_stub_%+%1:
     call exception_handler
-    iret 
+    iretq
 %endmacro
 ; if writing for 64-bit, use iretq instead
 %macro isr_no_err_stub 1
 isr_stub_%+%1:
     call exception_handler
-    iret
+    iretq
 %endmacro
 
 
