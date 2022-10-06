4f002353c72b5b31ace2131aa4a441121f8e27cf
Tristan Adams <killerdragonxs@gmail.com>
2022-10-06 13:44:33 -0400

    Update asmUtils.asm

--- src/asmUtils.asm
+++ src/asmUtils.asm
@@ -1,8 +1,19 @@
 [bits 64]
 
 global breakpoint
+global stop_interrupts
+global start_interrupts
 
 ; Bochs Magic Breakpoints
 breakpoint:
     xchg bx,bx
+    ret
+
+
+stop_interrupts:
+    cli
+    ret
+
+start_interrupts:
+    sti 
     ret
