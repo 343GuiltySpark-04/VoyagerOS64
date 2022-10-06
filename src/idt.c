aed9bf7092b8d42f609c65215fcc140fdb8f2ff6
Tristan Adams <killerdragonxs@gmail.com>
2022-10-06 11:55:02 -0400

    Update idt.c

--- src/idt.c
+++ src/idt.c
@@ -1,6 +1,11 @@
 #include "include/global_defs.h"
 #include <stdint.h>
 #include "include/idt.h"
+#include <stdbool.h>
+
+idt_entry_t idt[IDT_MAX_DESCRIPTORS];
+
+static bool vectors[IDT_MAX_DESCRIPTORS];
 
 __attribute__((aligned(0x10))) static idt_entry_t idt[256]; // Create an array of IDT entries; aligned for performance
 typedef struct
@@ -41,19 +46,22 @@ void idt_set_descriptor(uint8_t vector, void *isr, uint8_t flags)
     descriptor->reserved = 0;
 }
 
+extern void *isr_stub_table[];
 
-extern void* isr_stub_table[];
- 
 void idt_init(void);
-void idt_init() {
+void idt_init()
+{
     idtr.base = (uintptr_t)&idt[0];
     idtr.limit = (uint16_t)sizeof(idt_entry_t) * IDT_MAX_DESCRIPTORS - 1;
- 
-    for (uint8_t vector = 0; vector < 32; vector++) {
+
+    for (uint8_t vector = 0; vector < 32; vector++)
+    {
         idt_set_descriptor(vector, isr_stub_table[vector], 0x8E);
         vectors[vector] = true;
     }
- 
-    __asm__ volatile ("lidt %0" : : "m"(idtr)); // load the new IDT
-    __asm__ volatile ("sti"); // set the interrupt flag
+
+    __asm__ volatile("lidt %0"
+                     :
+                     : "m"(idtr)); // load the new IDT
+    __asm__ volatile("sti");       // set the interrupt flag
 }
