3f3dbcee9e3bbe2d12f27581b68a45564943d126
Tristan Adams <killerdragonxs@gmail.com>
2022-10-05 21:04:58 -0400

    v

--- src/idt.c
+++ src/idt.c
@@ -2,16 +2,7 @@
 #include <stdint.h>
 #include "include/idt.h"
 
-typedef struct
-{
-    uint16_t isr_low;   // The lower 16 bits of the ISR's address
-    uint16_t kernel_cs; // The GDT segment selector that the CPU will load into CS before calling the ISR
-    uint8_t ist;        // The IST in the TSS that the CPU will load into RSP; set to zero for now
-    uint8_t attributes; // Type and attributes; see the IDT page
-    uint16_t isr_mid;   // The higher 16 bits of the lower 32 bits of the ISR's address
-    uint32_t isr_high;  // The higher 32 bits of the ISR's address
-    uint32_t reserved;  // Set to zero
-} PACKED idt_entry_t;
+
 
 __attribute__((aligned(0x10))) static idt_entry_t idt[256]; // Create an array of IDT entries; aligned for performance
 
@@ -27,4 +18,17 @@ __attribute__((noreturn)) void exception_handler(void);
 void exception_handler()
 {
     __asm__ volatile("cli; hlt"); // Completely hangs the computer
+}
+
+
+void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags) {
+    idt_desc_t* descriptor = &idt[vector];
+ 
+    descriptor->base_low       = (uint64_t)isr & 0xFFFF;
+    descriptor->cs             = 0x0008;
+    descriptor->ist            = 0;
+    descriptor->attributes     = flags;
+    descriptor->base_mid       = ((uint64_t)isr >> 16) & 0xFFFF;
+    descriptor->base_high      = ((uint64_t)isr >> 32) & 0xFFFFFFFF;
+    descriptor->rsv0           = 0;
 }
