dcc5b531eb1d9a14a9299000a7b5ce317a79d698
Tristan Adams <killerdragonxs@gmail.com>
2022-10-06 11:56:48 -0400

    kk

--- src/idt.c
+++ src/idt.c
@@ -9,24 +9,6 @@ idt_entry_t idt[IDT_MAX_DESCRIPTORS];
 
 static bool vectors[IDT_MAX_DESCRIPTORS];
 
-__attribute__((aligned(0x10))) static idt_entry_t idt[256]; // Create an array of IDT entries; aligned for performance
-typedef struct
-{
-    uint16_t base_low;  // The lower 16 bits of the ISR's address
-    uint16_t cs;        // The GDT segment selector that the CPU will load into CS before calling the ISR
-    uint8_t ist;        // The IST in the TSS that the CPU will load into RSP; set to zero for now
-    uint8_t attributes; // Type and attributes; see the IDT page
-    uint16_t base_mid;  // The higher 16 bits of the lower 32 bits of the ISR's address
-    uint32_t base_high; // The higher 32 bits of the ISR's address
-    uint32_t reserved;  // Set to zero
-} PACKED idt_entry_t;
-
-typedef struct
-{
-    uint16_t limit;
-    uint64_t base;
-} PACKED idtr_t;
-
 static idtr_t idtr;
 
 __attribute__((noreturn)) void exception_handler(void);
