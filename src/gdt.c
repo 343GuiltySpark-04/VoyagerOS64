5c88a93c8b91c98887cd2de45cfdebf561f7e322
Tristan Adams <killerdragonxs@gmail.com>
2022-10-05 20:34:43 -0400

    Update gdt.c

--- src/gdt.c
+++ src/gdt.c
@@ -53,6 +53,13 @@ struct GDT_Entry User_ds = {
 
 };
 
+struct TSS_Entry TSS = {
+
+    .base = &TSS,
+    .limit = sizeof(TSS)
+
+};
+
 struct GDT_Desc desc = {
 
     .limit = sizeof(null_seg) + sizeof(Kernel_cs) + sizeof(Kernel_ds) + sizeof(User_cs) + sizeof(User_ds) - 1,
