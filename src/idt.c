f644b1c2011ae6c0bb8cff21ed7a807a408d7fab
Tristan Adams <killerdragonxs@gmail.com>
2022-10-05 21:05:24 -0400

    Update idt.c

--- src/idt.c
+++ src/idt.c
@@ -2,8 +2,6 @@
 #include <stdint.h>
 #include "include/idt.h"
 
-
-
 __attribute__((aligned(0x10))) static idt_entry_t idt[256]; // Create an array of IDT entries; aligned for performance
 
 typedef struct
@@ -20,15 +18,15 @@ void exception_handler()
     __asm__ volatile("cli; hlt"); // Completely hangs the computer
 }
 
+void idt_set_descriptor(uint8_t vector, void *isr, uint8_t flags)
+{
+    idt_desc_t *descriptor = &idt[vector];
 
-void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags) {
-    idt_desc_t* descriptor = &idt[vector];
- 
-    descriptor->base_low       = (uint64_t)isr & 0xFFFF;
-    descriptor->cs             = 0x0008;
-    descriptor->ist            = 0;
-    descriptor->attributes     = flags;
-    descriptor->base_mid       = ((uint64_t)isr >> 16) & 0xFFFF;
-    descriptor->base_high      = ((uint64_t)isr >> 32) & 0xFFFFFFFF;
-    descriptor->rsv0           = 0;
+    descriptor->base_low = (uint64_t)isr & 0xFFFF;
+    descriptor->cs = 0x0008;
+    descriptor->ist = 0;
+    descriptor->attributes = flags;
+    descriptor->base_mid = ((uint64_t)isr >> 16) & 0xFFFF;
+    descriptor->base_high = ((uint64_t)isr >> 32) & 0xFFFFFFFF;
+    descriptor->rsv0 = 0;
 }
