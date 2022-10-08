3aaa9eedfe96a3089a903a6ff8a8ee93a2b4f939
Tristan Adams <killerdragonxs@gmail.com>
2022-10-08 13:41:50 -0400

    Update gdt.h

--- src/include/gdt.h
+++ src/include/gdt.h
@@ -23,62 +23,35 @@ enum GDTAccessFlag
 #define GDTAccessUserCode (ReadWrite | Execute | Segments | GDTAccessDPL(3) | Present)
 #define GDTAccessUserData (ReadWrite | Segments | GDTAccessDPL(3) | Present)
 
-typedef struct PACKED GDT_Desc
-{
-    uint16_t size;
-    uint64_t offset;
-};
-
 typedef struct PACKED GDT_Entry
 {
-    uint16_t limit_low;
-    uint16_t base_low;
-    uint8_t base_middle;
-    uint8_t access_flag;
-    uint8_t limit_flags;
-    uint8_t base_high;
-};
+    uint16_t base;
+    uint32_t limit;
+    uint8_t access_byte;
+    uint8_t flags;
+} GDT_Entry_t;
 
 typedef struct PACKED TSS_Entry
 {
-    uint16_t length;
-    uint16_t base_low;
-    uint8_t base_mid;
-    uint8_t flags;
-    uint8_t flags2;
-    uint8_t base_high;
-    uint32_t base_up;
-    uint32_t reserved0;
-};
 
-struct PACKED TSS
-{
-    uint32_t reserved0;
+    uint64_t base;
+    uint16_t limit;
+    uint8_t access_byte;
+    uint8_t flags;
     uint64_t rsp0;
-    uint64_t rsp1;
-    uint64_t rsp2;
-    uint64_t reserved1;
     uint64_t ist1;
     uint64_t ist2;
-    uint64_t ist3;
-    uint64_t ist4;
-    uint64_t ist5;
-    uint64_t ist6;
-    uint64_t ist7;
-    uint64_t reserved2;
-    uint16_t reserved3;
-    uint16_t iopb_offset;
-};
+} TSS_Entry_t;
 
-struct PACKED ALIGN_4K GDT
+typedef struct PACKED GDT
 {
-    struct GDT_Entry null;
-    struct GDT_Entry kernelCS;
-    struct GDT_Entry kernelData;
-    struct GDT_Entry userNull;
-    struct GDT_Entry userData;
-    struct GDT_Entry userCode;
-    struct TSS_Entry tss;
-}GDT;
+
+    GDT_Entry_t null_seg;
+
+} GDT_t;
+
+extern GDT;
+extern GDT_Entry;
+extern TSS_Entry;
 
 void LoadGDT_Stage1();
