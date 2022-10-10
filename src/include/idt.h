9c4891890128257073ee56410a5520cd84112d40
Tristan Adams <killerdragonxs@gmail.com>
2022-10-10 17:32:56 -0400

    Update idt.h

--- src/include/idt.h
+++ src/include/idt.h
@@ -30,3 +30,23 @@ typedef struct PACKED IDT_Entry
     uint32_t reserved;
 
 } idt_entry_t;
+
+struct PACKED cpu_state
+{
+
+    uint64_t rax;
+    uint64_t rbx;
+    uint64_t rcx;
+    uint64_t rdx;
+    uint64_t rsi;
+    uint64_t rdi;
+    uint64_t rbp;
+    uint64_t r8;
+    uint64_t r9;
+    uint64_t r10;
+    uint64_t r11;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+};
