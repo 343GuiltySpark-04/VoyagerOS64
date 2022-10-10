9c4039cf0b69052dc8af0ed31d460810d053980c
Tristan Adams <killerdragonxs@gmail.com>
2022-10-10 18:39:03 -0400

    Update interrupts.h

--- src/include/interrupts.h
+++ src/include/interrupts.h
@@ -2,6 +2,47 @@
 #include <stdint.h>
 #include "global_defs.h"
 
+#define PIC1 0x20 // Master PIC
+#define PIC2 0xA0 // Slave PIC
+#define PIC1_DATA (PIC1 + 1)
+#define PIC2_DATA (PIC2 + 1)
+#define PIC_EOI 0x20 // end of interrupt
+#define IRQ_BASE 0x20
+
+// Exceptions, cf. http://wiki.osdev.org/Exceptions
+#define EXCEPTION_DE 0
+#define EXCEPTION_DB 1
+#define EXCEPTION_BP 3
+#define EXCEPTION_OF 4
+#define EXCEPTION_BR 5
+#define EXCEPTION_UD 6
+#define EXCEPTION_NM 7
+#define EXCEPTION_DF 8
+#define EXCEPTION_TS 10
+#define EXCEPTION_NP 11
+#define EXCEPTION_SS 12
+#define EXCEPTION_GP 13
+#define EXCEPTION_PF 14
+// 15 is "reserved"
+#define EXCEPTION_MF 16
+#define EXCEPTION_AC 17
+// ...
+
+// Hardware interrupts
+#define IRQ0 32
+#define IRQ1 33
+#define IRQ2 34
+#define IRQ3 35
+#define IRQ4 36
+#define IRQ5 37
+#define IRQ6 38
+#define IRQ7 39
+#define IRQ8 40
+#define IRQ9 41
+#define IRQ10 42
+#define IRQ11 43
+#define IRQ12 44
+
 struct PACKED cpu_state
 {
 
