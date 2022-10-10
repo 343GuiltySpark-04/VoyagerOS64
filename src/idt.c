1dc2a7079e4a35c66cce532bc0ebc7d802f9cd75
Tristan Adams <killerdragonxs@gmail.com>
2022-10-10 19:14:41 -0400

    h

--- src/idt.c
+++ src/idt.c
@@ -4,6 +4,7 @@
 #include "include/global_defs.h"
 #include "include/idt.h"
 #include "include/printf.h"
+#include "include/gdt.h"
 
 ALIGN_16BIT static idt_entry_t idt[IDT_ENTRIES];
 
@@ -21,4 +22,17 @@ void irq_handler()
 
     printf_("%s\n", "!!!KERNEL PANIC!!!");
     printf_("%s", "INTERRUPT HANDLING NOT AVIAL!");
+}
+
+void idt_set_descriptor(uint8_t vector, void *isr, uint8_t flags)
+{
+    idt_entry_t *descriptor = &idt[vector];
+
+    descriptor->base_low = (uint64_t)isr & 0xFFFF;
+    descriptor->cs = GDTKernelBaseSelector;
+    descriptor->ist = 0;
+    descriptor->attributes = flags;
+    descriptor->base_mid = ((uint64_t)isr >> 16) & 0xFFFF;
+    descriptor->base_high = ((uint64_t)isr >> 32) & 0xFFFFFFFF;
+    descriptor->rsv0 = 0;
 }
--- src/include/idt.h
+++ src/include/idt.h
@@ -9,13 +9,13 @@
 
 typedef struct PACKED IDT_Entry
 {
-    uint16_t isr_low;   // The lower 16 bits of the ISR's address
-    uint16_t kernel_cs; // The GDT segment selector that the CPU will load into CS before calling the ISR
+    uint16_t base_low;  // The lower 16 bits of the ISR's address
+    uint16_t cs;        // The GDT segment selector that the CPU will load into CS before calling the ISR
     uint8_t ist;        // The IST in the TSS that the CPU will load into RSP; set to zero for now
     uint8_t attributes; // Type and attributes; see the IDT page
-    uint16_t isr_mid;   // The higher 16 bits of the lower 32 bits of the ISR's address
-    uint32_t isr_high;  // The higher 32 bits of the ISR's address
-    uint32_t reserved;  // Set to zero
+    uint16_t base_mid;  // The higher 16 bits of the lower 32 bits of the ISR's address
+    uint32_t base_high; // The higher 32 bits of the ISR's address
+    uint32_t rsv0;      // Set to zero
 } idt_entry_t;
 
 typedef struct PACKED IDTR_Desc
@@ -23,3 +23,5 @@ typedef struct PACKED IDTR_Desc
     uint16_t limit;
     uint64_t base;
 } idtr_t;
+
+void idt_set_descriptor(uint8_t vector, void *isr, uint8_t flags);
