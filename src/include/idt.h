c6157a32efcf7dca957ec8fdb8d7dff8220723d6
Tristan Adams <killerdragonxs@gmail.com>
2022-10-10 18:08:31 -0400

    n

--- src/include/idt.h
+++ src/include/idt.h
@@ -5,3 +5,28 @@
 #define IDT_INTERRUPT_GATE 0xE
 #define IDT_TRAP_GATE 0xF
 #define IDT_ENTRIES 256
+
+typedef struct PACKED IDT_Entry
+{
+
+    uint16_t offset_low;
+    uint16_t selector;
+    uint8_t ist;
+
+    union
+    {
+
+        struct
+        {
+            uint8_t type : 4;
+            uint8_t s : 1;
+            uint8_t dpl : 2;
+            uint8_t present : 1;
+        };
+    };
+
+    uint16_t offset_mid;
+    uint32_t offset_high;
+    uint32_t reserved;
+
+} idt_entry_t;
--- src/include/interrupts.h
+++ src/include/interrupts.h
@@ -2,31 +2,6 @@
 #include <stdint.h>
 #include "global_defs.h"
 
-typedef struct PACKED IDT_Entry
-{
-
-    uint16_t offset_low;
-    uint16_t selector;
-    uint8_t ist;
-
-    union
-    {
-
-        struct
-        {
-            uint8_t type : 4;
-            uint8_t s : 1;
-            uint8_t dpl : 2;
-            uint8_t present : 1;
-        };
-    };
-
-    uint16_t offset_mid;
-    uint32_t offset_high;
-    uint32_t reserved;
-
-} idt_entry_t;
-
 struct PACKED cpu_state
 {
 
