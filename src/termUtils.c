15fb27f96984b3d2f65d443fec161b5bff356a70
Tristan Adams <killerdragonxs@gmail.com>
2022-10-16 22:11:01 -0400

    modified:   src/termUtils.c

--- src/termUtils.c
+++ src/termUtils.c
@@ -3,11 +3,16 @@
 #include "include/printf.h"
 #include "include/string.h"
 #include "include/kernel.h"
+#include "include/liballoc.h"
 
 void *alloc_mem(size_t size)
 {
+
+    malloc((uint64_t)size);
+    // memset(); zero memory?
 }
 void free_mem(void *ptr, size_t size)
 {
-    // Free memory
+
+    free(ptr); // i feel like theres more??
 }
