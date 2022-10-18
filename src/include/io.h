710079eba65a1675a89b51dfeb368763e5147dff
Tristan Adams <killerdragonxs@gmail.com>
2022-10-17 22:32:58 -0400

    added inb and outb

---
+++ src/include/io.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <stdint.h>
+
+#ifndef _IO_H
+#define _IO_H
+
+/** inb:
+ *  Read a byte from an I/O port.
+ *
+ *  @param  port The address of the I/O port
+ *  @return      The read byte
+ */
+uint8_t inb(uint16_t port);
+
+/** outb:
+ *  Sends the given data to the given I/O port. Defined in io.s
+ *
+ *  @param port The I/O port to send the data to
+ *  @param data The data to send to the I/O port
+ */
+void outb(uint16_t port, uint8_t data);
+
+#endif
