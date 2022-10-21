f30f7301f5d72cb1be4a74e03dc4f749c3ef2012
Tristan Adams <killerdragonxs@gmail.com>
2022-10-21 16:31:22 -0400

    dwf

--- src/interrupts.c
+++ src/interrupts.c
@@ -3,3 +3,54 @@
 #include "include/idt.h"
 #include "include/printf.h"
 #include "include/interrupts.h"
+#include "include/io.h"
+
+char *exception_messages[32] = {
+    "Division By Zero",
+    "Debug",
+    "Non Maskable Interrupt",
+    "Breakpoint",
+    "Overflow",
+    "BOUND Range Exceeded",
+    "Invalid Opcode",
+    "Device Not Available (No Math Coprocessor)",
+    "Double Fault",
+    "Coprocessor Segment Overrun",
+    "Invalid TSS",
+    "Segment Not Present",
+    "Stack-Segment Fault",
+    "General Protection",
+    "Page Fault",
+    "Unknown Interrupt (intel reserved)",
+    "x87 FPU Floating-Point Error (Math Fault)",
+    "Alignment Check",
+    "Machine Check",
+    "SIMD Floating-Point Exception",
+    "Virtualization Exception",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved"};
+
+void isr_handler()
+{
+}
+
+void pic8259_init(){
+
+
+outb(PIC1_CMD, ICW1);
+outb(PIC2_CMD, ICW1);
+
+
+
+
+
+}
--- src/isr.asm
+++ src/isr.asm
@@ -10,8 +10,15 @@ isr_stub_%+%1:
     iretq
 %endmacro
 
+%macro isr_req_stub 1
+isr_stub_%+%1:
+    call isr_handler
+    iretq
+%endmacro
+
 
 extern exception_handler
+extern isr_handler
 isr_no_err_stub 0
 isr_no_err_stub 1
 isr_no_err_stub 2
@@ -44,11 +51,28 @@ isr_no_err_stub 28
 isr_no_err_stub 29
 isr_err_stub    30
 isr_no_err_stub 31
+isr_req_stub 32
+isr_req_stub 33
+isr_req_stub 34
+isr_req_stub 35
+isr_req_stub 36
+isr_req_stub 37
+isr_req_stub 38
+isr_req_stub 39
+isr_req_stub 40
+isr_req_stub 41
+isr_req_stub 42
+isr_req_stub 43
+isr_req_stub 44
+isr_req_stub 45
+isr_req_stub 46
+isr_req_stub 47
+
 
 global isr_stub_table
 isr_stub_table:
 %assign i 0 
-%rep    32 
+%rep    48 
     dq isr_stub_%+i ; use DQ instead if targeting 64-bit
 %assign i i+1 
 %endrep
