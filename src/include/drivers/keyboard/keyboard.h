1c3101e79b957f977182adcab53e7cc5bb23f0a2
Tristan Adams <killerdragonxs@gmail.com>
2026-09-09 16:15:26 -0400

    Tighten keyboard interface prototypes

--- src/include/drivers/keyboard/keyboard.h
+++ src/include/drivers/keyboard/keyboard.h
@@ -3,10 +3,7 @@
 #define KEYBOARD_DATA_PORT 0x60
 #define KEYBOARD_STATUS_PORT 0x64
 
-void keyboard_handler();
-
-void keyboard_init();
-
-char k_getchar();
-
-char kbd_pop();
+void keyboard_handler(void);
+void keyboard_init(void);
+char k_getchar(void);
+char kbd_pop(void);
