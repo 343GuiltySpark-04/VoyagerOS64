8087c9ba09df007d81d428dc77ac4863acef2adf
Tristan Adams <killerdragonxs@gmail.com>
2026-09-09 16:14:26 -0400

    Fix VSH interface types

--- src/include/shell.h
+++ src/include/shell.h
@@ -6,8 +6,8 @@
 #define VSH_CMD_BUFFER_SIZE 100
 #define VSH_VERSION "0.0.1"
 
-void vsh_loop();
-
-char vsh_readline();
+void  vsh_loop(void);
+char *vsh_readline(void);
+void  cmd_parser(const char *str);
 
 #endif
