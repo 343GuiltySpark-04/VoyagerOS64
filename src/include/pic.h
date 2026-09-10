56cceb52076f410f9f74da9c42740cff51180426
Tristan Adams <killerdragonxs@gmail.com>
2026-09-09 21:15:23 -0400

    Document PIC IRQ and EOI contract

--- src/include/pic.h
+++ src/include/pic.h
@@ -1,3 +1,8 @@
+/**
+ * @file pic.h
+ * @brief Legacy 8259 PIC programming interface.
+ * @ingroup interrupts
+ */
 #pragma once
 #include "stdint.h"
 
@@ -20,8 +25,35 @@
 #define PIC_ICW4_BUF_SLAVE 0x08
 #define PIC_ICW4_BUF_MASTER 0x0C
 
+/**
+ * @brief Initialize/remap the legacy PICs for Voyager's interrupt layout.
+ */
 void pic_enable(void);
+
+/**
+ * @brief Remap PIC IRQ0 to a new IDT vector base.
+ * @param irq_offset Vector number corresponding to IRQ0 after remapping.
+ */
 void pic_remap_offsets(uint8_t irq_offset);
+
+/**
+ * @brief Mask one legacy PIC IRQ line.
+ * @param irq IRQ number in the range handled by the cascaded PICs.
+ */
 void pic_mask_irq(uint8_t irq);
+
+/**
+ * @brief Unmask one legacy PIC IRQ line.
+ * @param irq IRQ number in the range handled by the cascaded PICs.
+ */
 void pic_unmask_irq(uint8_t irq);
+
+/**
+ * @brief Send end-of-interrupt acknowledgement for one legacy PIC IRQ.
+ * @param irq Hardware IRQ number, not an arbitrary IDT vector.
+ *
+ * The generic interrupt dispatcher only sends PIC EOI for vectors belonging to
+ * the remapped hardware IRQ range. Dynamic/software vectors must not be passed
+ * through PIC acknowledgement merely because they used the same dispatcher.
+ */
 void pic_send_eoi(uint8_t irq);
